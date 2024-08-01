2ba7fb45786d61bec366c36bff80d242a789b06c
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-08-01 23:31:36 +0200

    add task-2 promises generator

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,53 @@
-console.log('Generator');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+const messageOptions = {
+  messageColor: '#FFF6F4',
+  position: 'topRight',
+  timeout: 3000,
+  animateInside: false,
+  progressBar: false,
+  transitionIn: 'fadeIn',
+  transitionOut: 'fadeOut',
+  close: false,
+};
+
+const handleSubmit = e => {
+  e.preventDefault();
+
+  const delay = form.elements.delay.value;
+  const state = form.elements.state.value;
+
+  form.reset();
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        backgroundColor: '#33C682',
+        ...messageOptions,
+      });
+    })
+    .catch(delay => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        backgroundColor: '#FF736A',
+        ...messageOptions,
+      });
+    });
+};
+
+submitBtn.addEventListener('click', handleSubmit);
